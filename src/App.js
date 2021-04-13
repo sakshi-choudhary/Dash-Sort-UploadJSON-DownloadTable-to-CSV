c5b200563686f32c312529eb5f36a6d46b4c84fa
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-13 12:01:20 +0530

    feat: create tables, download csv, pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,143 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CSVLink } from "react-csv";
+import PropTypes from "prop-types";
+import { makeStyles, withStyles, useTheme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(
+  name,
+  deviceType,
+  serial,
+  deviceId,
+  floor,
+  space,
+  commissioned
+) {
+  return { name, deviceType, serial, deviceId, floor, space, commissioned };
+}
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
+const headers = [
+  { label: "status", key: "status" },
+  { label: "name", key: "name" },
+  { label: "deviceType", key: "deviceType" },
+  { label: "serial", key: "serial" },
+  { label: "deviceId", key: "deviceId" },
+  { label: "space", key: "space" },
+  { label: "commissioned", key: "commissioned" },
+];
 const useSortableData = (items, config = null) => {
   const [sortConfig, setSortConfig] = React.useState(config);
 
@@ -36,18 +172,12 @@ const useSortableData = (items, config = null) => {
   return { items: sortedItems, requestSort, sortConfig };
 };
 
-const headers = [
-  { label: "status", key: "status" },
-  { label: "name", key: "name" },
-  { label: "deviceType", key: "deviceType" },
-  { label: "serial", key: "serial" },
-  { label: "deviceId", key: "deviceId" },
-  { label: "space", key: "space" },
-  { label: "commissioned", key: "commissioned" },
-];
-
 const ProductTable = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState("");
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
   useEffect(() => {
     axios
       .get(
@@ -55,6 +185,7 @@ const ProductTable = () => {
       )
       .then((res) => {
         console.log(res.data);
+        console.log(res.data.length);
         setProducts(res.data);
       })
       .catch((err) => {
@@ -66,6 +197,34 @@ const ProductTable = () => {
     headers: headers,
     filename: "ExportedTable.csv",
   };
+  const arr = [];
+
+  for (var i = 0; i < products.length; i++) {
+    arr.push(
+      createData(
+        `${products[i].name}`,
+        `${products[i].deviceType}`,
+        `${products[i].serial}`,
+        `${products[i].deviceId}`,
+        `${products[i].floor}`,
+        `${products[i].space}`,
+        `${products[i].commissioned ? "yes" : "no"}`
+      )
+    );
+  }
+  const rows = arr.sort((a, b) => (a.name < b.name ? -1 : 1));
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   const { items, requestSort, sortConfig } = useSortableData(products);
   const getClassNamesFor = (name) => {
     if (!sortConfig) {
@@ -75,7 +234,76 @@ const ProductTable = () => {
   };
   return (
     <>
-      <table>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="custom pagination table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Device Name</StyledTableCell>
+              <StyledTableCell align="right">Device Type</StyledTableCell>
+              <StyledTableCell align="right">Serial Number</StyledTableCell>
+              <StyledTableCell align="right">Device Id</StyledTableCell>
+              <StyledTableCell align="right">Floor</StyledTableCell>
+              <StyledTableCell align="right">Space</StyledTableCell>
+              <StyledTableCell align="right">Commisioned</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.deviceType}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.serial}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.deviceId}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.floor}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.space}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  {row.commissioned}
+                </TableCell>
+              </TableRow>
+            ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[10, 15, 20, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+      {/* <table>
         <caption className="text-3xl text-gray-500 py-2 shadow-lg font-bold">
           Products
         </caption>
@@ -130,7 +358,7 @@ const ProductTable = () => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
       <button className="flex justify-content-center items-center p-3 bg-blue-400 rounded-lg m-5 text-white font-bold hover:bg-blue-900">
         <CSVLink {...csvReport}> Export to CSV</CSVLink>
       </button>
