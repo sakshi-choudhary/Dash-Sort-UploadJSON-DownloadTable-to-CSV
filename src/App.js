d8c7917e20dda400b289a654d141b234d868729b
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-14 18:15:08 +0530

    upload json file

--- src/App.js
+++ src/App.js
@@ -45,24 +45,8 @@ const App = () => {
   );
   const [main, setMain] = useState(true);
   const [tab2, setTab2] = useState(false);
-
   const [data, setData] = useState([]);
-  const [skipPageReset, setSkipPageReset] = useState(false);
 
-  const updateMyData = (rowIndex, columnId, value) => {
-    setSkipPageReset(true);
-    setData((item) =>
-      item.map((row, index) => {
-        if (index === rowIndex) {
-          return {
-            ...item[rowIndex],
-            [columnId]: value,
-          };
-        }
-        return row;
-      })
-    );
-  };
   const headers = [
     { label: "status", key: "status" },
     { label: "Name", key: "name" },
@@ -78,11 +62,43 @@ const App = () => {
     headers: headers,
     filename: "ExportedTable.csv",
   };
+  let fileReader;
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+    //console.log(content);
+    return JSON.parse(content);
+  };
+
+  const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+
+    fileReader.readAsText(file);
+  };
+  const onFileJson = () => {
+    const x = handleFileRead();
+    setData(x);
+  };
 
   return (
     <div>
       <CssBaseline />
       <div className="justify-center bg-gray-50 flex shadow-md">
+        <div>
+          <input
+            type="file"
+            id="file"
+            accept=".json"
+            className="m-3"
+            onChange={(e) => handleFileChosen(e.target.files[0])}
+          />
+          <br />
+          <button
+            className=" p-3 bg-blue-400 mb-1  rounded-lg mt-2 text-white font-bold hover:bg-blue-900"
+            onClick={onFileJson}
+          >
+            upload JSON file
+          </button>
+        </div>
         <div>
           <button className="flex justify-content-center items-center p-3 bg-blue-400 rounded-lg mt-2 mx-3 text-white font-bold hover:bg-blue-900">
             <CSVLink {...csvReport}> Export to CSV</CSVLink>
@@ -135,13 +151,7 @@ const App = () => {
             autoFocus
           />
         </form>
-        <EnhancedTable
-          columns={columns}
-          data={data}
-          setData={setData}
-          updateMyData={updateMyData}
-          skipPageReset={skipPageReset}
-        />
+        <EnhancedTable columns={columns} data={data} setData={setData} />
       </div>
       <div
         className={`${
