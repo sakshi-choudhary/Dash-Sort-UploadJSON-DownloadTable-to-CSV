a5f049efd5b9c37da691f903f5142ef5e95b9ed3
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-14 18:29:38 +0530

    style

--- src/App.js
+++ src/App.js
@@ -84,22 +84,6 @@ const App = () => {
       <CssBaseline />
       <div className="justify-center bg-gray-50 flex shadow-md">
         <div>
-          <input
-            type="file"
-            id="file"
-            accept=".json"
-            className="m-3"
-            onChange={(e) => handleFileChosen(e.target.files[0])}
-          />
-          <br />
-          <button
-            className=" p-3 bg-blue-400 mb-1  rounded-lg mt-2 text-white font-bold hover:bg-blue-900"
-            onClick={onFileJson}
-          >
-            upload JSON file
-          </button>
-        </div>
-        <div>
           <button className="flex justify-content-center items-center p-3 bg-blue-400 rounded-lg mt-2 mx-3 text-white font-bold hover:bg-blue-900">
             <CSVLink {...csvReport}> Export to CSV</CSVLink>
           </button>{" "}
@@ -136,21 +120,45 @@ const App = () => {
         </div>
       </div>
       <div className={`${main ? "block" : "hidden"} mt-5`}>
-        <form className="mx-10">
-          <TextField
-            rowsMin={5}
-            value={data}
-            onChange={(e) => setData(JSON.parse(e.target.value))}
-            variant="outlined"
-            fullWidth
-            multiline
-            id="feedback"
-            label="Paste JSON here"
-            name="text"
-            rows={10}
-            autoFocus
-          />
-        </form>
+        <div className="flex md:flex-row flex-col justify-items-center items-center mx-auto">
+          <form className="mx-10">
+            <TextField
+              rowsMin={5}
+              value={data}
+              onChange={(e) => setData(JSON.parse(e.target.value))}
+              variant="outlined"
+              fullWidth
+              multiline
+              id="feedback"
+              label="Paste JSON here"
+              name="text"
+              rows={10}
+              className="w-full"
+              autoFocus
+            />
+          </form>
+          <div>
+            <h1 className="m-2 font-extrabold text-4xl bg-blue-300 w-20">OR</h1>
+            <div className="border-3 p-3 m-3 border">
+              <label>Upload a JSON file</label>
+              <br />
+              <input
+                type="file"
+                id="file"
+                accept=".json"
+                className="m-3"
+                onChange={(e) => handleFileChosen(e.target.files[0])}
+              />
+              <br />
+              <button
+                className=" p-3 bg-blue-400 mb-1  rounded-lg mt-2 text-white font-bold hover:bg-blue-900"
+                onClick={onFileJson}
+              >
+                upload
+              </button>
+            </div>
+          </div>
+        </div>
         <EnhancedTable columns={columns} data={data} setData={setData} />
       </div>
       <div
